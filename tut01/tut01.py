53faccefa5ef88b138b979dfff06c5a7eac68a9f
Vipin Dhakad <dhakadvipin301@gmail.com>
2022-09-14 18:12:38 +0530

    Added the average values and the deviation of every value from its average

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,9 +1,33 @@
 # tut1 solution
-def factorial(a):
-    if(a==1):
-        return 1
-    return factorial(a-1)*a
-a=int(input("Enter a number for finding the factorial..."))
-fact=factorial(a)
+import csv
+import pandas as pd
+with open('octant_input.csv','r') as input_file:
+    reader=csv.reader(input_file)
+    with open('output.csv','w',newline='') as output_file:
+        writer=csv.writer(output_file)
+        for row in reader:
+            writer.writerow(row)
+data=pd.read_csv('output.csv')
+u_avg=data['U'].mean()
+v_avg=data['V'].mean()
+w_avg=data['W'].mean()
+print(u_avg)
+print(v_avg)
+print(w_avg)
+# data['U_avg']=""
+# data['V_avg']=""
+# data['W_avg']=""
+data.at[0,'U_avg']=u_avg
+data.at[0,'V_avg']=v_avg
+data.at[0,'W_avg']=w_avg
+i=0
+for ele in data['U']:
+    data.at[i,"U'=U-Uavg"]=data.at[i,'U']-u_avg
+    data.at[i,"U'=U-Vavg"]=data.at[i,'V']-v_avg
+    data.at[i,"U'=U-Wavg"]=data.at[i,'W']-w_avg
+    i=i+1
 
-print(fact)
+
+
+data.to_csv('output.csv',index=False)
+
