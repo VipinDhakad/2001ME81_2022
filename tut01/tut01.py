697b4332a9bd0f01139ed497dd19e1c7fc3766e7
Vipin Dhakad <dhakadvipin301@gmail.com>
2022-09-14 20:13:05 +0530

    Added Mod value count logic in code part

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -21,13 +21,112 @@ data.at[0,'U_avg']=u_avg
 data.at[0,'V_avg']=v_avg
 data.at[0,'W_avg']=w_avg
 i=0
+cnt_p1=0
+cnt_n1=0
+cnt_p2=0
+cnt_n2=0
+cnt_p3=0
+cnt_n3=0
+cnt_p4=0
+cnt_n4=0
+
 for ele in data['U']:
-    data.at[i,"U'=U-Uavg"]=data.at[i,'U']-u_avg
-    data.at[i,"U'=U-Vavg"]=data.at[i,'V']-v_avg
-    data.at[i,"U'=U-Wavg"]=data.at[i,'W']-w_avg
+    x=data.at[i,"U'=U-Uavg"]=data.at[i,'U']-u_avg
+    y=data.at[i,"V'=V-Vavg"]=data.at[i,'V']-v_avg
+    z=data.at[i,"W'=W-Wavg"]=data.at[i,'W']-w_avg
+    if x>0:
+        if y>0:
+            if z>0:
+                data.at[i,'Octant']=1
+                cnt_p1=cnt_p1+1
+            else:
+                data.at[i,'Octant']=-1
+                cnt_n1=cnt_n1+1
+        else:
+            if z>0:
+                data.at[i,'Octant']=4
+                cnt_p4=cnt_p4+1
+            else:
+                data.at[i,'Octant']=-4
+                cnt_n4=cnt_n4+1
+    else:
+        if y>0:
+            if z>0:
+                data.at[i,'Octant']=2
+                cnt_p2=cnt_p2+1
+            else:
+                data.at[i,'Octant']=-2
+                cnt_n2=cnt_n2+1
+        else:
+            if z>0:
+                data.at[i,'Octant']=3
+                cnt_p3=cnt_p3+1
+            else:
+                data.at[i,'Octant']=-3
+                cnt_n3=cnt_n3+1
     i=i+1
+data.at[0,'Octant ID']='Overall Count'
+data.at[0,'1']=cnt_p1
+data.at[0,'-1']=cnt_n1
+data.at[0,'2']=cnt_p2
+data.at[0,'-2']=cnt_n2
+data.at[0,'3']=cnt_p3
+data.at[0,'-3']=cnt_n3
+data.at[0,'4']=cnt_p4
+data.at[0,'-4']=cnt_n4
 
-
+mod=5000
+i=0
+prev=-1
+iter=1
+cnt_p1=0
+cnt_n1=0
+cnt_p2=0
+cnt_n2=0
+cnt_p3=0
+cnt_n3=0
+cnt_p4=0
+cnt_n4=0
+data.at[1,'Octant ID']=mod
+while i<len(data):
+    for j in range(prev+1,mod*iter+1):
+        if i<len(data):
+            if data.at[i,'Octant']==1:
+                cnt_p1=cnt_p1+1
+            if data.at[i,'Octant']==-1:
+                cnt_n1=cnt_n1+1
+            if data.at[i,'Octant']==2:
+                cnt_p2=cnt_p2+1
+            if data.at[i,'Octant']==-2:
+                cnt_n2=cnt_n2+1
+            if data.at[i,'Octant']==3:
+                cnt_p3=cnt_p3+1
+            if data.at[i,'Octant']==-3:
+                cnt_n3=cnt_n3+1
+            if data.at[i,'Octant']==4:
+                cnt_p4=cnt_p4+1
+            if data.at[i,'Octant']==-4:
+                cnt_n4=cnt_n4+1
+        
+        data.at[iter+1,'1']=cnt_p1
+        data.at[iter+1,'-1']=cnt_n1
+        data.at[iter+1,'2']=cnt_p2
+        data.at[iter+1,'-2']=cnt_n2
+        data.at[iter+1,'3']=cnt_p3
+        data.at[iter+1,'-3']=cnt_n3
+        data.at[iter+1,'4']=cnt_p4
+        data.at[iter+1,'-4']=cnt_n4
+        cnt_p1=0
+        cnt_n1=0
+        cnt_p2=0
+        cnt_n2=0
+        cnt_p3=0
+        cnt_n3=0
+        cnt_p4=0
+        cnt_n4=0
+        i=i+1
+    prev=mod*iter
+    iter=iter+1
 
 data.to_csv('output.csv',index=False)
 
