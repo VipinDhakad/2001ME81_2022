ff9e19df957add9667cf19eda79b0a717cda675f
Vipin Dhakad <84377420+VipinDhakad@users.noreply.github.com>
2022-10-15 16:59:40 +0530

    Added comments

--- tut05/tut05.py
+++ tut05/tut05.py
@@ -1,9 +1,6 @@
-
-
+#Vipin Kumar Dhakad 2001ME81
 from datetime import datetime
 start_time = datetime.now()
-
-#Help https://youtu.be/N6PBd4XdnEw
 import pandas as pd
 import openpyxl
 from datetime import datetime
@@ -84,7 +81,7 @@ def octant_range_names(mod=5000):
                     data.at[i,'Octant']=-3
                     cnt_n3=cnt_n3+1
         i=i+1
-    data.at[0,'Octant ID']='Overall Count'
+    data.at[0,'Octant ID']='Overall Count'   #writing the octant values obtained
     data.at[0,'1']=cnt_p1
     data.at[0,'-1']=cnt_n1
     data.at[0,'2']=cnt_p2
@@ -93,7 +90,7 @@ def octant_range_names(mod=5000):
     data.at[0,'-3']=cnt_n3
     data.at[0,'4']=cnt_p4
     data.at[0,'-4']=cnt_n4
-
+  ##octant tagging for mod based range
     i=0
     prev=0
     iter=1
@@ -126,7 +123,7 @@ def octant_range_names(mod=5000):
         data.at[iter+1,'Octant ID']=str(prev)+"-"+str(mod*iter-1)
         if flag:
             data.at[iter+1,'Octant ID']=str(prev)+"-"+str(len(data))
-        data.at[iter+1,'1']=cnt_p1
+        data.at[iter+1,'1']=cnt_p1         ##writing the values obtained
         data.at[iter+1,'-1']=cnt_n1
         data.at[iter+1,'2']=cnt_p2
         data.at[iter+1,'-2']=cnt_n2
@@ -150,16 +147,16 @@ def octant_range_names(mod=5000):
     for octant in octants:
         data.at[0,f'Rank of {octant}']=f'Rank {i}'
         i+=1
-        list_of_octant_counts.append((data.at[0,f'{octant}'],octant))
-    list_of_octant_counts.sort(reverse=True)
+        list_of_octant_counts.append((data.at[0,f'{octant}'],octant))   #creating a list with count and octant id
+    list_of_octant_counts.sort(reverse=True)          ##sorting the list in descending order
     i=1
     for item in list_of_octant_counts:
         data.at[1,f'Rank of {item[1]}']=i
         i+=1
     data.at[1,'Rank1 Octant ID']=list_of_octant_counts[0][1]
-    data.at[1,'Rank 1 Octant Name']=octant_name_id_mapping[f"{list_of_octant_counts[0][1]}"]
+    data.at[1,'Rank 1 Octant Name']=octant_name_id_mapping[f"{list_of_octant_counts[0][1]}"]         ##setting the octant name
     row=2
-    Rank1_mod_values_count=[]
+    Rank1_mod_values_count=[]   ##finding the rank1 octant for mod based range values
     while row-1<iter:
         list_of_octant_counts.clear()
         for octant in octants:
@@ -174,7 +171,7 @@ def octant_range_names(mod=5000):
         Rank1_mod_values_count.append(list_of_octant_counts[0][1])
         row+=1
     print('Finished Calculations')
-    row=13
+    row=13                          #generating the summary of rank1 values
     data.iloc[12,12]='Octant ID'
     data.iloc[12,13]='Octant Name'
     data.iloc[12,14]='Count of Rank 1 MOD Values'
@@ -183,14 +180,13 @@ def octant_range_names(mod=5000):
         data.at[row,'-1']=octant_name_id_mapping[f"{octant}"]
         data.at[row,'2']=Rank1_mod_values_count.count(octant)
         row+=1
-    data.to_excel('output.xlsx',index=False)
+    data.to_excel('output.xlsx',index=False)        #saving the output
 
-###Code
 
 from platform import python_version
 ver = python_version()
 
-if ver == "3.8.10":
+if ver == "3.8.10":              #checking the version of python installed
     print("Correct Version Installed")
 else:
     print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
@@ -201,6 +197,6 @@ octant_range_names(mod)
 
 
 
-#This shall be the last lines of the code.
+#This shall be the last lines of the code ... and these are
 end_time = datetime.now()
 print('Duration of Program Execution: {}'.format(end_time - start_time))
