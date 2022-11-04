eafbf6850e1769fd6168c43a8aefe7df835f3ee8
Vipin Dhakad <84377420+VipinDhakad@users.noreply.github.com>
2022-11-05 02:22:42 +0530

    Added code

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -1,6 +1,7 @@
 import csv
 import pandas as pd
 from datetime import datetime
+import openpyxl
 start_time = datetime.now()
 
 def attendance_report():
@@ -22,78 +23,76 @@ def attendance_report():
     mc=sum(1 for row in open("input_registered_students.csv"))
     mc_consolidated=sum(1 for row in open("input_attendance.csv"))
     total_dates=list()
-    for i in range(0,mc_consolidated-1):
-        if inp.at[i,'Timestamp'].split()[0] not in total_dates:
-            total_dates.append(inp.at[i,'Timestamp'].split()[0])
+    for j in range(0,mc_consolidated-1):
+        day=inp.at[j,'Timestamp'].split()[0].split('-')[0]
+        month=inp.at[j,'Timestamp'].split()[0].split('-')[1]
+        year=inp.at[j,'Timestamp'].split()[0].split('-')[2]
+        date=datetime.strptime(f'{year}-{month}-{day}', "%Y-%m-%d").date()
+        day_name=date.strftime("%A")
+        if day_name=="Monday" or day_name=="Thursday":
+            if inp.at[j,'Timestamp'].split()[0] not in total_dates:
+                total_dates.append(inp.at[j,'Timestamp'].split()[0])
     # max_att=0
     # max_roll=""
-    fileName_consolidated=".\myoutput\\attendance_report_consolidated.csv"
-    fileName_duplicate=".\myoutput\\attendance_report_duplicate.csv"
-    with open(fileName_consolidated,'w',newline='') as output_file:
-        writer=csv.writer(output_file)
-        header = ['Roll', 'Name', 'total_lecture_taken', 'attendance_count_actual','attendance_count_fake','attendance_count_absent','Percentage']
-        writer.writerow(header)
-    with open(fileName_duplicate,'w',newline='') as output_file:
-        writer=csv.writer(output_file)
-        header = ["Timestamp","Roll","Name","Total count of attendance on that day"]
-        writer.writerow(header)
-    attend_consolidated=pd.read_csv(fileName_consolidated)
-    attend_duplicate=pd.read_csv(fileName_duplicate)
-    duplicate_index=0
-    for i in range(0,mc-1):
-        duplicated_attendance=0
-        rollno=rollno_inp.at[i,'Roll No']
-        t_lec,t_lec_act,t_lec_fake,t_lec_abs,percent=len(total_dates),0,0,0,0
-        for j in range(0,mc_consolidated-1):
-            unique_dates=list()
-            if inp.at[j,'Attendance'].split()[0]==rollno:
-                day=inp.at[j,'Timestamp'].split()[0][0:2]
-                month=inp.at[j,'Timestamp'].split()[0][3:5]
-                year=inp.at[j,'Timestamp'].split()[0][6:10]
-                date=datetime.strptime(f'{year}-{month}-{day}', "%Y-%m-%d").date()
-                day_name=date.strftime("%A")
+    fileName_consolidated=".\myoutput\\attendance_report_consolidated.xlsx"
 
-                if day_name=='Monday' or day_name=='Thursday':
-                    if (inp.at[j,'Timestamp'].split()[1].split(':')[0] == '14'):
-                        if inp.at[j,'Timestamp'].split()[0] not in unique_dates:
-                            unique_dates.append(inp.at[j,'Timestamp'].split()[0])
-                            t_lec_act+=1
+    output_file=openpyxl.Workbook()
+    output=output_file.active
+    output_file.save(fileName_consolidated)
+    
+    attend_consolidated=pd.read_excel(fileName_consolidated)
+    for i in range(0,mc-1):
+        total_Present=0
+        date_index=1
+        rollno=rollno_inp.at[i,'Roll No']
+        fileName=".\myoutput\\"+rollno+'.xlsx'
+        output_file=openpyxl.Workbook()
+        output=output_file.active
+        output_file.save(fileName)
+        out=pd.read_excel(fileName)
+        for sp_date in total_dates:
+            duplicated_attendance=0
+            t_lec,t_lec_act,t_lec_fake,t_lec_abs,percent=len(total_dates),0,0,0,0
+            t_lec_count=0
+            for j in range(0,mc_consolidated-1):
+                if inp.at[j,'Attendance'].split()[0]==rollno:
+                    if inp.at[j,'Timestamp'].split()[0] == sp_date:
+                        t_lec_count+=1
+                        time=inp.at[j,'Timestamp'].split()[1]
+                        hour=time.split(':')[0]
+                        minutes=time.split(':')[1]
+                        if ((hour=='14') or (hour=='15' and minutes=='00')):
+                            if t_lec_act==0:
+                                t_lec_act+=1
+                            else:
+                                duplicated_attendance+=1
                         else:
-                            attend_duplicate.at[duplicate_index,'Timestamp']=inp.at[j,'Timestamp']
-                            attend_duplicate.at[duplicate_index,'Roll']=rollno
-                            attend_duplicate.at[duplicate_index,'Name']=rollno_inp.at[i,'Name']
-                            duplicated_attendance+=1
-                    else:
-                        t_lec_fake+=1
-                else: t_lec_fake+=1
-        attend_duplicate.at[duplicate_index,'Total count of attendance on that day']=duplicated_attendance
-        fileName=".\myoutput\\"+rollno+'.csv'
-        with open(fileName,'w',newline='') as output_file:
-            writer=csv.writer(output_file)
-            header = ['Roll', 'Name', 'total_lecture_taken', 'attendance_count_actual','attendance_count_fake','attendance_count_absent','Percentage']
-            writer.writerow(header)
-        out=pd.read_csv(fileName)
-        out.at[0,'Roll']=rollno
-        attend_consolidated.at[i,'Roll']=rollno
-        out.at[0,'Name']=rollno_inp.at[i,'Name']
-        attend_consolidated.at[i,'Name']=rollno_inp.at[i,'Name']
-        out.at[0,'total_lecture_taken']=t_lec
-        attend_consolidated.at[i,'total_lecture_taken']=t_lec
-        out.at[0,'attendance_count_actual']=t_lec_act
-        attend_consolidated.at[i,'attendance_count_actual']=t_lec_act
-        # if t_lec_act>max_att:
-        #     max_att=t_lec_act
-        #     max_roll=rollno
-        out.at[0,'attendance_count_fake']=t_lec_fake
-        attend_consolidated.at[i,'attendance_count_fake']=t_lec_fake
-        out.at[0,'attendance_count_absent']= t_lec_abs = t_lec-t_lec_act
-        attend_consolidated.at[i,'attendance_count_absent']= t_lec_abs = t_lec-t_lec_act
-        out.at[0,'Percentage']=round((t_lec_act/t_lec)*100,2)
-        attend_consolidated.at[i,'Percentage']=round((t_lec_act/t_lec)*100,2)
-        out.to_csv(fileName,index=False)
-    attend_consolidated.to_csv(fileName_consolidated,index=False)
-    attend_duplicate.to_csv(fileName_duplicate,index=False)
-    # print(max_roll,max_att)
+                            t_lec_fake+=1
+            
+            out.at[0,'Roll']=rollno
+            out.at[0,'Name']=rollno_inp.at[i,'Name']
+            out.at[date_index,'Dates']=sp_date
+            attend_consolidated.at[i+1,'Roll']=rollno
+            attend_consolidated.at[i+1,'Name']=rollno_inp.at[i,'Name']
+            out.at[date_index,'Total Attendance Count']=t_lec_count
+            attend_consolidated.at[i+1,f'{sp_date}']='P' if t_lec_act>0 else 'A'
+            if t_lec_act>0: total_Present+=1 
+            out.at[date_index,'Real']=t_lec_act
+            out.at[date_index,'Duplicate']=duplicated_attendance
+            out.at[date_index,'Invalid']= t_lec_fake
+            out.at[date_index,'Absent']=1 if t_lec_act==0 else 0
+            date_index+=1
+        attend_consolidated.at[i+1,'Actual Lecture Taken']=len(total_dates)
+        attend_consolidated.at[i+1,'Total Real']=total_Present
+        attend_consolidated.at[i+1,'% Attendance']=round(total_Present/len(total_dates)*100,2)
+        out.to_excel(fileName,index=False)
+        if i==int(0.2*mc): print("20% /complete")
+        if i==int(0.4*mc): print("40% /complete")
+        if i==int(0.6*mc): print("60% /complete")
+        if i==int(0.8*mc): print("80% /complete")
+        if i==int(mc-2): print("100% /complete")
+
+    attend_consolidated.to_excel(fileName_consolidated,index=False)
 # ,,,,,, (attendance_count_actual/total_lecture_taken) 2 digit decimal 
 
 from platform import python_version
