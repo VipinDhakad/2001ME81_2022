48480195740d41847b571b439fec0524d36b2539
Vipin Dhakad <84377420+VipinDhakad@users.noreply.github.com>
2022-11-05 22:18:22 +0530

    Final Touches
    Added comments and changed the output directory from myoutput to output

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -5,22 +5,22 @@ import openpyxl
 start_time = datetime.now()
 
 def attendance_report():
-    try:
+    try:                              ##file reading
         inp_file = pd.read_csv('input_attendance.csv')
-        inp = inp_file.fillna("2001CCXX Random")
+        inp = inp_file.fillna("2001CCXX Random") ##filling the empty cells with garbage value
     except:
         print("File not found")
     
     
     try:
-        rollno_inp=pd.read_csv('input_registered_students.csv')
+        rollno_inp=pd.read_csv('input_registered_students.csv')          ##reading the registered student file
     except:
         print('File containing name of all students is missing!')
     
 
     # mr = sheet_input.max_row
 
-    mc=sum(1 for row in open("input_registered_students.csv"))
+    mc=sum(1 for row in open("input_registered_students.csv"))         ##getting the size of dataframe
     mc_consolidated=sum(1 for row in open("input_attendance.csv"))
     total_dates=list()
     for j in range(0,mc_consolidated-1):
@@ -31,44 +31,44 @@ def attendance_report():
         day_name=date.strftime("%A")
         if day_name=="Monday" or day_name=="Thursday":
             if inp.at[j,'Timestamp'].split()[0] not in total_dates:
-                total_dates.append(inp.at[j,'Timestamp'].split()[0])
-    # max_att=0
-    # max_roll=""
-    fileName_consolidated=".\myoutput\\attendance_report_consolidated.xlsx"
+                total_dates.append(inp.at[j,'Timestamp'].split()[0]) ##getting the total dates on which lectures were held
+    
+                                                                    ##setting the name of output files
+    fileName_consolidated=".\output\\attendance_report_consolidated.xlsx"
 
     output_file=openpyxl.Workbook()
     output=output_file.active
     output_file.save(fileName_consolidated)
     
     attend_consolidated=pd.read_excel(fileName_consolidated)
-    for i in range(0,mc-1):
+    for i in range(0,mc-1):  ##looping the registered students name
         total_Present=0
         date_index=1
         rollno=rollno_inp.at[i,'Roll No']
-        fileName=".\myoutput\\"+rollno+'.xlsx'
+        fileName=".\output\\"+rollno+'.xlsx'
         output_file=openpyxl.Workbook()
         output=output_file.active
         output_file.save(fileName)
         out=pd.read_excel(fileName)
-        for sp_date in total_dates:
+        for sp_date in total_dates: ##looping through the dates on which lectures were held
             duplicated_attendance=0
             t_lec,t_lec_act,t_lec_fake,t_lec_abs,percent=len(total_dates),0,0,0,0
             t_lec_count=0
             for j in range(0,mc_consolidated-1):
-                if inp.at[j,'Attendance'].split()[0]==rollno:
+                if inp.at[j,'Attendance'].split()[0]==rollno:  ##if the roll no. matches
                     if inp.at[j,'Timestamp'].split()[0] == sp_date:
                         t_lec_count+=1
                         time=inp.at[j,'Timestamp'].split()[1]
                         hour=time.split(':')[0]
                         minutes=time.split(':')[1]
-                        if ((hour=='14') or (hour=='15' and minutes=='00')):
+                        if ((hour=='14') or (hour=='15' and minutes=='00')):  ##if the timing is within the lecture timings
                             if t_lec_act==0:
                                 t_lec_act+=1
                             else:
                                 duplicated_attendance+=1
                         else:
                             t_lec_fake+=1
-            
+            ##setting the data begins here
             out.at[0,'Roll']=rollno
             out.at[0,'Name']=rollno_inp.at[i,'Name']
             out.at[date_index,'Dates']=sp_date
@@ -82,10 +82,14 @@ def attendance_report():
             out.at[date_index,'Invalid']= t_lec_fake
             out.at[date_index,'Absent']=1 if t_lec_act==0 else 0
             date_index+=1
+
+        ##setting the data into the consolidated file
         attend_consolidated.at[i+1,'Actual Lecture Taken']=len(total_dates)
         attend_consolidated.at[i+1,'Total Real']=total_Present
         attend_consolidated.at[i+1,'% Attendance']=round(total_Present/len(total_dates)*100,2)
         out.to_excel(fileName,index=False)
+
+        ##loader for percentage completion of the process
         if i==int(0.2*mc): print("20% /complete")
         if i==int(0.4*mc): print("40% /complete")
         if i==int(0.6*mc): print("60% /complete")
@@ -97,7 +101,7 @@ def attendance_report():
 
 from platform import python_version
 ver = python_version()
-
+##version checking of the python
 if ver == "3.8.10":
     print("Correct Version Installed")
 else:
