a79e636420a49fb728540ced87210419ffd38b4c
Vipin Dhakad <84377420+VipinDhakad@users.noreply.github.com>
2022-11-30 23:53:59 +0530

    updated code for file transfer

--- proj1/client.py
+++ proj1/client.py
@@ -4,7 +4,14 @@ import tkinter
 from tkinter import scrolledtext
 from tkinter import simpledialog 
 import time
+import easygui
+from tkinter import filedialog as fd
 from tkinter import *
+# import tqdm
+import os
+
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 4096 # send 4096 bytes each time step
 
 HOST='127.0.0.1'
 PORT=9090
@@ -13,8 +20,8 @@ class Client:
 
     def __init__(self,host, port):
 
-        self.sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.sock.connect((host,port))
+        self.s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.s.connect((host,port))
 
         msg=tkinter.Tk()
         msg.withdraw()
@@ -33,7 +40,7 @@ class Client:
         RecvThread.start()
     def ui(self):
         self.window=tkinter.Tk()
-        self.window.geometry('650x630')
+        self.window.geometry('650x660')
         self.win=tkinter.PanedWindow(self.window)           #creating two sections in the main window
         self.win.configure(bg="lightgray")
         self.win.pack(fill=BOTH, expand=1)
@@ -65,12 +72,16 @@ class Client:
         self.msgs_label.config(font=("Arial",12))     #giving a label to the message box area
         self.msgs_label.pack(padx=20,pady=5)
 
-        self.msg_input=tkinter.Text(self.chat_area,height=3, )
+        self.msg_input=tkinter.Text(self.chat_area,height=3)
         self.msg_input.pack(padx=20,pady=5)
 
         self.send_btn=tkinter.Button(self.chat_area,text="Send",command=self.write_msg)
-        self.send_btn.config(font=("Arial",12))       ##button to send the message
+        self.send_btn.config(font=("Arial",12))       ##button to send the file
         self.send_btn.pack(padx=20,pady=5)
+
+        self.file_btn=tkinter.Button(self.chat_area,text="Attach",command=self.send_file)
+        self.file_btn.config(font=("Arial",12))       ##button to send the message
+        self.file_btn.pack(padx=20,pady=5)
 
         self.exit_btn=tkinter.Button(self.chat_area,text="EXIT CHAT",command=self.exit)
         self.exit_btn.config(font=("Arial",12))            ##exit button to leave chat
@@ -79,25 +90,47 @@ class Client:
         self.ui_complete=True
         self.window.protocol("WM_DELETE_WINDOW",self.exit)
         self.win.mainloop()
+    
+    def send_file(self):
+        print("pressed the send file button")
+        fileName=easygui.fileopenbox()
+        fileSize = os.path.getsize(fileName)
+        print('got filename and size')
+        self.s.send("!FTP".encode('utf-8'))
+        print('sent file transfer hint to srver')
+        self.s.send(f"{fileName}{SEPARATOR}{fileSize}".encode('utf-8'))
+        print('sent filename and size to server')
+        with open(fileName, "rb") as f:
+            while True:
+                print('sending...')
+                # read the bytes from the file
+                bytes_read = f.read()
+                if not bytes_read:
+                    # file transmitting is done
+                    print('done sending')
+                    break
+                # we use sendall to assure transimission in 
+                # busy networks
+                self.s.sendall(bytes_read)
 
     def write_msg(self):                      ##sends the message to server and server sends it to all clients
         message=f"{self.username}: {self.msg_input.get('1.0','end')}"
-        self.sock.send(message.encode('utf-8'))
+        self.s.send(message.encode('utf-8'))
         self.msg_input.delete('1.0','end')
     def exit(self):                                             #exit method; executed when either exit button
         # self.sock.send("exit")                      #    is pressed or 'X' is pressed
         # self.sock.send(f'{self.username}')
         self.running=False
         self.window.destroy()
-        self.sock.close()
+        self.s.close()
         exit(0)
     def receive(self):
         while self.running:
             try:
-                msg_b=self.sock.recv(1024)
+                msg_b=self.s.recv(1024)
                 msg=str(msg_b,encoding='utf-8')
                 if msg=='USERNAME':                         #sends the username of the client
-                    self.sock.send(self.username.encode('utf-8'))
+                    self.s.send(self.username.encode('utf-8'))
                     msg=""
                 else:
                     if msg=="c":
@@ -134,7 +167,7 @@ class Client:
                 break
             except:
                 print("Unknown Error!!!")
-                self.sock.close()
+                self.s.close()
                 break
 
 
--- proj1/server.py
+++ proj1/server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import time
+import os
 
 HOST='127.0.0.1'
 PORT=9090
@@ -9,6 +10,9 @@ server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind((HOST,PORT))
 
 server.listen()
+
+BUFFER_SIZE = 4096
+SEPARATOR = "<SEPARATOR>"
 
 clients=[]
 nicknames=[]
@@ -22,8 +26,34 @@ def handle(client):
     while True:
         try:
             message=client.recv(1024)
-            broadcast("m".encode('utf-8'))
-            broadcast(message)
+            if message.decode('utf-8')=='!FTP':
+                print('from SERVER: received !FTP')
+                received = client.recv(1024).decode('utf-8')
+                fileName, fileSize = received.split(SEPARATOR)
+                print(f'received {fileName} and {fileSize}')
+                # remove absolute path if there is
+                fileName = os.path.basename(fileName)
+                # convert to integer
+                fileSize = int(fileSize)
+                with open(fileName, "wb") as f:
+                    while True:
+                        # read 1024 bytes from the socket (receive)
+                        bytes_read = client.recv(BUFFER_SIZE)
+                        if not bytes_read:    
+                            # nothing is received
+                            # file transmitting is done
+                            # broadcast(f)
+                            break
+                        # write to the file the bytes we just received
+                        print('wrote into file...')
+                        f.write(bytes_read)
+                        broadcast(bytes_read)
+                print("Done receiving the file\n")
+                # broadcast("m".encode('utf-8'))
+                # broadcast(f"{fileName} Received!")
+            else:
+                broadcast("m".encode('utf-8'))
+                broadcast(message)
         except:
             index=clients.index(client)
             clients.remove(client)
